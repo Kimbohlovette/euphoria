8067fe9430a4f4f5b03ca67b6c96c7b9fab3d24c
kimbohlovette <kimbohlovette@gmail.com>
2024-04-06 02:44:28 +0100

    Get all products endpoint done

---
+++ api/src/controllers/product/get_all_products.ts
@@ -0,0 +1,23 @@
+import { Request, Response } from 'express';
+import { Product } from '../../models/models';
+
+export const getAllProducts = async (req: Request, res: Response) => {
+	try {
+		if (!!req.query.category) {
+			const products = await Product.findAll({
+				where: {
+					category: req.query.category,
+				},
+			});
+			return res.status(200).json(products);
+		}
+		const products = await Product.findAll();
+		return res.status(200).json(products);
+	} catch (err) {
+		return res.status(500).json({
+			message: 'Error getting products',
+			error: err,
+			code: 'internal_server_error',
+		});
+	}
+};
--- api/src/routes/product_routes.ts
+++ api/src/routes/product_routes.ts
@@ -1,8 +1,10 @@
 import { Router } from 'express';
 import { createProduct } from '../controllers/product/create_product';
+import { getAllProducts } from '../controllers/product/get_all_products';
 
 const router = Router();
 
 router.post('/', createProduct);
+router.get('/', getAllProducts);
 
 export default router;
