1cb54560a5597ca9189a4842ee25ebc3338d9dd4
kimbohlovette <kimbohlovette@gmail.com>
2024-04-08 21:56:01 +0100

    (dashboard) added bg gradient to add to pro card

--- dashboard/src/components/upgrade_to_pro.tsx
+++ dashboard/src/components/upgrade_to_pro.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const UpgradeToPro = () => {
 	return (
-		<div className="px-5 py-5 md:px-8 rounded-3xl bg-violet-800 flex flex-col items-center gap-5">
+		<div className="px-5 py-5 md:px-8 rounded-3xl bg-gradient-to-r from-violet-950 via-violet-900 to-violet-800 flex flex-col items-center gap-5">
 			<h1 className="text-white font-semibold text-center">
 				Upgrade to PRO get access to all Features!
 			</h1>
