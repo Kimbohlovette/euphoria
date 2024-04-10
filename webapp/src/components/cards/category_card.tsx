2f23e0c6362ad8734e5511a0b8937d7cddb663ce
kimbohlovette <kimbohlovette@gmail.com>
2024-04-10 07:47:38 +0100

    (webapp) implemented the category

--- webapp/src/components/cards/category_card.tsx
+++ webapp/src/components/cards/category_card.tsx
@@ -1,5 +1,31 @@
-const CategoryCard = () => {
-	return <div>CategoryCard</div>;
+import { NavLink } from "react-router-dom";
+
+interface Props {
+	imageUrl: string;
+	actionText?: string;
+	title: string;
+}
+const CategoryCard = ({ imageUrl, actionText, title }: Props) => {
+	return (
+		<div className="overflow-hidden">
+			<img
+				className="w-full sm:aspect-square h-72 object-cover rounded-lg"
+				src={imageUrl}
+				alt=""
+			/>
+			<div className="mt-2">
+				<h3 className="font-medium text-gray-800">{title}</h3>
+				{actionText && (
+					<NavLink
+						to={`/products?cat=${title}`}
+						className="text-gray-600 text-sm"
+					>
+						{actionText}
+					</NavLink>
+				)}
+			</div>
+		</div>
+	);
 };
 
 export default CategoryCard;
